1bf6d2dafb0e1628d376e97f60bb65448202809f
wizzydev <wisdomanaba83@gmail.com>
2020-01-24 09:57:25 +0100

    Updates

---
+++ precache-manifest.085b3848424c74a91dc5340aa00d03fe.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb536a8362a62daf456c93ece4ab3685",
+    "url": "/react-gh-pages/index.html"
+  },
+  {
+    "revision": "916fa5a6b41024785e9b",
+    "url": "/react-gh-pages/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "651907a7e02234f7cfcc",
+    "url": "/react-gh-pages/static/js/2.33af256b.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react-gh-pages/static/js/2.33af256b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "916fa5a6b41024785e9b",
+    "url": "/react-gh-pages/static/js/main.10731745.chunk.js"
+  },
+  {
+    "revision": "ecfd1d276f9241e07c49",
+    "url": "/react-gh-pages/static/js/runtime-main.d24436ce.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-gh-pages/static/media/logo.5d5d9eef.svg"
+  }
+]);
